2761a6f963051d3c5dcf5a77d4d526a77fe36038
edgaroviedo23 <edgaroviedojbs@gmail.com>
2025-05-26 19:42:15 +0200

    .env funcionando nuevo

--- miportafolio/miportafolio/settings.py
+++ miportafolio/miportafolio/settings.py
@@ -13,8 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+from dotenv import load_dotenv
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -56,7 +60,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 ROOT_URLCONF = 'miportafolio.urls'
 
 TEMPLATES = [
@@ -91,6 +95,8 @@ WSGI_APPLICATION = 'miportafolio.wsgi.application'
 DATABASES = {
     'default' : dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
